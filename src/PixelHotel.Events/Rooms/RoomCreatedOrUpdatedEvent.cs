33a00fbaaa2cfe93e33e979642bee58715c02b94
Wesley Costa <wesley_costa@outlook.com>
2024-06-18 16:45:39 -0300

    code style

--- src/PixelHotel.Events/Rooms/RoomCreatedOrUpdatedEvent.cs
+++ src/PixelHotel.Events/Rooms/RoomCreatedOrUpdatedEvent.cs
@@ -1,6 +1,4 @@
 ﻿using PixelHotel.Core.Events;
-using System;
-using System.Drawing;
 
 namespace PixelHotel.Events.Rooms;
 
@@ -8,18 +6,4 @@ public class RoomCreatedOrUpdatedEvent : Event
 {
     public string Name { get; set; }
     public int Number { get; set; }
-
-    public RoomCreatedOrUpdatedEvent()
-    {
-
-    }
-
-    public RoomCreatedOrUpdatedEvent(Guid id,
-        string name,
-        int number)
-    {
-        AggregateId = id;
-        Name = name;
-        Number = number;
-    }
 }
