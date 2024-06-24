d088323d5a150c1be89a54cce2148f96aa9bb097
Wesley Costa <wesley_costa@outlook.com>
2024-06-24 14:54:04 -0300

    1.4.1

---
+++ src/Orangotango.Core/Bus/PublisherEventBase.cs
@@ -0,0 +1,19 @@
+﻿using Orangotango.Core.Abstractions;
+using Orangotango.Core.Enums;
+using Orangotango.Core.Events;
+
+namespace Orangotango.Core.Bus;
+
+public abstract class PublisherEventBase(string _eventName,
+    ILoggerService _logger)
+{
+    protected void LogPublishEvent<TEvent>(TEvent @event) where TEvent : Event
+    {
+        var message = $"Event published {_eventName}";
+
+        _logger.Information(nameof(OperationLogs.EventPublished),
+            message,
+            @event,
+            @event.TranceId);
+    }
+}
--- src/Orangotango.Infra/Bus/PublisherEvent.cs
+++ src/Orangotango.Infra/Bus/PublisherEvent.cs
@@ -1,20 +1,22 @@
 ﻿using MassTransit;
 using Orangotango.Core.Abstractions;
 using Orangotango.Core.Bus.Abstractions;
+using Orangotango.Core.Enums;
 using System;
 using System.Threading.Tasks;
 using Event = Orangotango.Core.Events.Event;
 
 namespace Orangotango.Infra.Bus;
 
-internal sealed class PublisherEvent(IBus _bus, ILoggerService _loggerService) : IPublisherEvent
+internal sealed class PublisherEvent(IBus _bus,
+    ILoggerService _logger) : IPublisherEvent
 {
     public async Task Publish<TEvent>(TEvent @event) where TEvent : Event
     {
         @event.Timestamp = DateTimeOffset.UtcNow;
         if (@event.TranceId == default)
         {
-            @event.TranceId = _loggerService.GetTraceId();
+            @event.TranceId = _logger.GetTraceId();
         }
 
         await _bus.Publish(@event);
