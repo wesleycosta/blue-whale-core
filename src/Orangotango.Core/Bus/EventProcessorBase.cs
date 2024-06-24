624a01c74c938a0bfdd04ceb95f0b6728fa755ea
Wesley Costa <wesley_costa@outlook.com>
2024-06-24 19:30:07 -0300

    version: 1.5.0

---
+++ src/Orangotango.Core/Bus/EventProcessorBase.cs
@@ -0,0 +1,17 @@
+﻿using Orangotango.Core.Abstractions;
+using Orangotango.Core.Enums;
+using Event = Orangotango.Core.Events.Event;
+
+namespace Orangotango.Core.Bus;
+
+public abstract class EventProcessorBase(ILoggerService _logger)
+{
+    protected void LogErrorProcessedWithFailure<TEvent>(TEvent @event) where TEvent : Event
+    {
+        var message = $"Event processed successfully {typeof(TEvent).Name}";
+
+        _logger.Information(nameof(OperationLogs.EventProcessedSuccessfully),
+            message,
+            @event.TranceId);
+    }
+}
