3ec071e8281cb86bae914e874b2887bae972eb42
Wesley Costa <wesley_costa@outlook.com>
2024-06-24 19:59:41 -0300

    version: 1.5.2

--- src/Orangotango.Infra/Bus/PublisherEvent.cs
+++ src/Orangotango.Infra/Bus/PublisherEvent.cs
@@ -1,5 +1,4 @@
 ﻿using MassTransit;
-using Orangotango.Core.Abstractions;
 using Orangotango.Core.Bus.Abstractions;
 using System;
 using System.Threading.Tasks;
@@ -7,14 +6,13 @@ using Event = Orangotango.Core.Events.Event;
 
 namespace Orangotango.Infra.Bus;
 
-internal sealed class PublisherEvent(IBus _bus,
-    ILoggerService _logger) : IPublisherEvent
+internal sealed class PublisherEvent(IBus _bus) : IPublisherEvent
 {
     public async Task Publish<TEvent>(TEvent @event) where TEvent : Event
     {
         if (@event.TranceId == Guid.Empty)
         {
-            @event.SetTraceId(_logger.GetTraceId());
+            @event.SetTraceId(Guid.NewGuid());
         }
 
         await _bus.Publish(@event);
