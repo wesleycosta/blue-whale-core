c4aa6936d8611d6858ca3d8a2a909dbdc8321cad
Wesley Costa <wesley_costa@outlook.com>
2024-06-28 17:45:53 -0300

    feat: EnumExtensions (v 1.9.0)

---
+++ src/Orangotango.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Orangotango.Core.Extensions;
+
+public static class EnumExtensions
+{
+    public static string GetDescription(this Enum value)
+    {
+        var field = value.GetType().GetField(value.ToString());
+        if (field is null)
+        {
+            return default;
+        }
+
+        var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+        return attribute == null ? value.ToString() : attribute.Description;
+    }
+}
+
